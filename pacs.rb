94d8fa91468faee84e3dfc638c125de40a8d1e94
Eike Bernhardt <eike.bernhardt@isn-oldenburg.de>
2009-09-17 17:06:23 +0200

    pax parser with stringscanner and all comments

--- pacs.rb
+++ pacs.rb
@@ -4,15 +4,17 @@
 require 'open-uri'
 require "strscan"
 require 'yaml'
+require 'pp'
 
 # PACSFILE = "http://www.aip.org/pacs/pacs08/ASCII2008FullPacs.txt"
 PACSFILE = "ASCII2008FullPacs.txt"
 
 class PACSParser
-  PACS = Struct.new(:id, :desc, :children)
+  PACS = Struct.new(:id, :desc, :children, :comment)
 
   def initialize
     @container = Array.new
+    @current = @parent = nil
   end
   
   def load
@@ -21,7 +23,8 @@ class PACSParser
       @lines = f.read
       f.close
       # sort, so that the ids are in the right order
-      @lines.sort{|a,b| a.casecmp(b)}.to_s
+      # @lines.sort{|a,b| a.casecmp(b)}.to_s
+      @lines
     rescue Exception => e
       puts "Error loading file: #{e}"
     end    
@@ -32,9 +35,17 @@ class PACSParser
     begin
       while !@input.eos?
         trim_space ||
-        parse_content
+        parse_toplevel ||
+        parse_secondlevel ||
+        parse_thirdlevel ||
+        parse_fourthlevel ||
+        parse_fifthlevel ||
+        parse_comment ||
+        ignore
       end
     rescue Exception => e
+      # pp @container
+      puts "Error in line: "+@input[0]
       raise
     end
   end
@@ -48,6 +59,11 @@ class PACSParser
   
   def print_pacs(pacs, indent = "")
     puts "#{indent}#{pacs.id} #{pacs.desc}"
+    if !pacs.comment.nil?
+      pacs.comment.each do |c|
+        puts "#{indent} ... ... #{c}"
+      end
+    end
     if !pacs.children.nil?
       pacs.children.keys.sort{ |a,b| a.to_s.casecmp(b.to_s) }.each do |k|
         print_pacs(pacs.children[k], indent+"\t") if !pacs.children[k].nil?
@@ -57,81 +73,96 @@ class PACSParser
   
   private
   
-  def parse_content
-    parse_pacs or
-    parse_comment or
-    ignore
-  end
-  
-  def parse_pacs
-    # FIXME: this really should be split into subroutines - but I couldn't care less ATM
-    if @input.scan(/(\d{2})\.(\d{2}).(..) (.*)\n/)
-      current_id = "#{@input[1]}.#{@input[2]}.#{@input[3]}"
-      desc = @input[4]
-      
-      if @input[1].to_i % 10 == 0
-        # top-level elements
-        @container.push PACS.new(current_id, desc)
-      elsif @input[2] == '00'
-        # second level categories
-        parent = @container[@input[1].to_i/10.round]
-        parent.children ||= {}
-        parent.children[@input[1].to_i] =  PACS.new(current_id, desc)
-      else
-        # third level and below
-        if @input[1] == '99'
-          # special handling for errata group (why is this a subgroup?!?? WTF?)
-          @container[9] = PACS.new(current_id, desc) if @container[9].nil?
-          @container[9][:children] ||= {}
-          @container[9][:children][@input[1].to_i] ||= PACS.new()
-        end
-
-        parent = @container[@input[1].to_i/10.round][:children][@input[1].to_i]
-        parent.children ||= {}
-        parent.children[current_id] =  PACS.new(current_id, desc)
-        
-        if @input[3].start_with?("-")
-          # start of level 4 group - scan ahead for level 3 ids with uppercase first characters
-          while !@input.eos? and @input.scan(/(\d{2})\.(\d{2}).([A-Z].) (.*)\n/)
-            l4_id = "#{@input[1]}.#{@input[2]}.#{@input[3]}"
-            l4_desc = @input[4]
-            
-            parent.children[current_id].children ||= {}
-            parent.children[current_id].children[l4_id] = PACS.new(l4_id, l4_desc)
-            
-            if @input[3].end_with?("-")
-              # start of a level 5 group
-              while !@input.eos? and @input.scan(/(\d{2})\.(\d{2}).([a-z].) (.*)\n/)
-                l5_id = "#{@input[1]}.#{@input[2]}.#{@input[3]}"
-                l5_desc = @input[4]
-                
-                parent.children[current_id].children[l4_id].children ||= {}
-                parent.children[current_id].children[l4_id].children[l5_id] = PACS.new(l5_id, l5_desc)
-              end
-            end
-          end
-        end
-      end
-      
-      
-      true
+  def parse_toplevel
+    if @input.scan(/(\d0)\.(\d{2})\.(..) (.*)\n/)
+      @current_id = "#{@input[1]}.#{@input[2]}.#{@input[3]}"
+      @desc = @input[4]
+      @container.push( PACS.new(@current_id, @desc, {}) )
+      @current = @container.last
     else
       false
     end
   end
   
+  def parse_secondlevel
+    if @input.scan(/(\d{2})\.(00)\.(..) (.*)\n/)
+      @current_id = "#{@input[1]}.#{@input[2]}.#{@input[3]}"
+      @desc = @input[4]
+      @parent = @container[@input[1].to_i/10.round]
+      @current = @parent.children[@current_id] = PACS.new(@current_id, @desc, {})
+    else
+      false
+    end
+  end
+
+  def parse_thirdlevel
+    if @input.scan(/(\d{2})\.(\d{2})\.(\+.) (.*)\n/) # normal 3rd level, just add
+      @current_3rd_lv = @current_id = "#{@input[1]}.#{@input[2]}.#{@input[3]}"
+      @desc = @input[4]
+      # this might end a subgroup. so re-set parent
+      @parent = @container[@input[1].to_i/10.round].children["#{@input[1]}.00.00"]
+      @current = @parent.children[@current_id] = PACS.new(@current_id, @desc, {})
+    elsif @input.scan(/(\d{2})\.(\d{2})\.(\-.) (.*)\n/) # opening of 4th level group
+      @current_3rd_lv = @current_id = "#{@input[1]}.#{@input[2]}.#{@input[3]}"
+      @desc = @input[4]
+
+      # errata group
+      if @input[1] == '99' and @container[9].children['99.00.00'].nil?
+        @container[9].children['99.00.00'] = PACS.new('','', {})
+      end
+
+      # this might end a subgroup. so re-set parent
+      @parent = @container[@input[1].to_i/10.round].children["#{@input[1]}.00.00"]
+      @current = @parent = @parent.children[@current_id] = PACS.new(@current_id, @desc, {})
+    else
+      false
+    end
+  end
+  
+  def parse_fourthlevel
+    if @input.scan(/(\d{2})\.(\d{2})\.([A-Z][^-]) (.*)\n/) # normal 4th level, just add
+      @current_id = "#{@input[1]}.#{@input[2]}.#{@input[3]}"
+      @desc = @input[4]
+      # this resets parent to level 4
+      @parent = @container[@input[1].to_i/10.round].children["#{@input[1]}.00.00"].children[@current_3rd_lv]
+      
+      @current = @parent.children[@current_id] = PACS.new(@current_id, @desc, {})
+    elsif @input.scan(/(\d{2})\.(\d{2})\.([A-Z]\-) (.*)\n/) # opening of 5th level group
+      @current_id = "#{@input[1]}.#{@input[2]}.#{@input[3]}"
+      @desc = @input[4]
+      # this resets parent to level 4
+      @parent = @container[@input[1].to_i/10.round].children["#{@input[1]}.00.00"].children[@current_3rd_lv]
+      
+      @current = @parent = @parent.children[@current_id] = PACS.new(@current_id, @desc, {})
+    else
+      false
+    end
+  end
+
+  def parse_fifthlevel
+    if @input.scan(/(\d{2})\.(\d{2})\.([a-z][a-z]) (.*)\n/) # normal 5th level, just add
+      @current_id = "#{@input[1]}.#{@input[2]}.#{@input[3]}"
+      @desc = @input[4]
+      @current = @parent.children[@current_id] = PACS.new(@current_id, @desc, {})
+    else
+      false
+    end
+  end
+    
   def parse_comment
-    if @input.scan(/\.{3} \.{3} (.*)\n/)
-      # do nothing for the moment
-      true
+    if @input.scan(/\.\.\. \.\.\. (.*)\n/)
+      # found a comment, append this to @current element
+      comment = @input[1]
+      @current.comment ||= []
+      @current.comment.push comment
     else
       false
     end
   end
   
   def ignore
-    if @input.scan(/(.*)\n/)
-      # puts "Ignoring '#{@input[1]}'"
+    if !@input.check(/..\...\... .*\n/) and !@input.check(/ \.{3} \.{3} .*\n/) and @input.scan(/(.*)\n/)
+      STDERR.puts "Ignoring '#{@input[1]}'"
     else
       false
     end
@@ -139,10 +170,6 @@ class PACSParser
   
   def trim_space
     @input.scan(/\s+/)
-  end
-  
-  def trim_newline
-    @input.scan(/\n/)
   end
   
   def error(message)
